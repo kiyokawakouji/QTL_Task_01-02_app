b572e402db8585b6162804be7a5d0be925689ea1
kiyokawakouji <kohji12k@gmail.com>
2022-04-04 01:49:07 +0900

    Containerで円形と四角を作成する

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,60 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Colors.blueAccent,
-        margin: const EdgeInsets.fromLTRB(10, 20, 30, 40),
+      body: Center(
+        child: Column(
+        children: <Widget>[
+          Container(
+            width: 300,
+            height: 300,
+            margin: const EdgeInsets.fromLTRB(10, 80, 10, 40),
+            decoration: const BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.blueAccent,
+            ),
+          ),
+          Container(
+            width: 300,
+            height: 300,
+            margin: const EdgeInsets.fromLTRB(10, 80, 10, 40),
+            decoration: const BoxDecoration(
+              shape: BoxShape.rectangle,
+              color: Colors.greenAccent,
+            ),
+          )
+        ],
+      ),
       ),
     );
   }
